9f9e41c7273fa9f2fdbffff473ce15c011409e30
mohan <mohansunam234@gmail.com>
2024-07-06 17:15:45 +0545

    react-project2

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header className='bg-black text-white flex'>
+        <h1>Tail Web</h1>
+        <nav>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
+
---
+++ src/pages/Home/Into.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Into = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Into
