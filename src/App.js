47bbcbd7636822b38a44e71481f85f58d12ec743
mohan <mohansunam234@gmail.com>
2024-07-06 19:05:38 +0545

    update 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import DisplaySec from './pages/Home/DisplaySec'
+import Tech from './pages/Home/Tech'
 const App = () => {
   return (
     <div>
       <Header />
       <DisplaySec />
+      <Tech />
+  
 
    
        
--- src/pages/Home/Tech.js
+++ src/pages/Home/Tech.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { DiAndroid, DiAngularSimple, DiApple, DiBootstrap, DiCss3, DiHtml5, DiJavascript, DiNodejs, DiPhp, DiPython } from 'react-icons/di'
 
 const Tech = () => {
   return (
     <div>
-      
+      <h1 className='text-3xl font-bold text-center'>Technologies I use</h1>
+      <div className="dev-icons grid grid-cols-4 justify-center">
+        <DiHtml5 color='blue' size={200} />
+        <DiAndroid color='blue' size={200}className='animate-spin'/>
+        <DiPython color='blue' size={200}/>
+        <DiNodejs color='blue' size={200}className='animate-bounce'/>
+        <DiBootstrap color='blue' size={200} className='animate-pulse'/>
+        <DiJavascript color='blue' size={200}/>
+        <DiPhp color='blue' size={200}className='animate-spin'/>
+        <DiAngularSimple color='blue' size={150}/>
+       
+
+      </div>
     </div>
   )
 }
