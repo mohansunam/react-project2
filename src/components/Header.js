b41b476f1c027129e59369f4c21ab68d968f77c5
mohan <mohansunam234@gmail.com>
2024-07-10 07:47:01 +0545

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <header className='bg-black text-white flex items-baseline justify-between px-2 py-4'>
-        <h1 className='text-2xl'>Tail Web</h1>
+        <h1 className='text-2xl'>Chandra Mohan sunam</h1>
         <nav className='space-x-5'>
           <a className='same' href="">About</a>
           <a className='same' href="">Contact</a>
